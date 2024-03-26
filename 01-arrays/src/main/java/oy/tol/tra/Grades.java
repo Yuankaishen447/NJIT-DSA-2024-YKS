62da40546cef64f5333e22277a23638066cb1413
Yuankaishem447 <2951317453@qq.com>
2024-03-26 09:09:35 +0800

    change01

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -32,14 +32,7 @@ public class Grades {
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
       /*int i = 0;*/
-      if (grades != null && grades.length != 0)
-      {
-      for ( int i=0; i < grades.length/2; i++) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-     }
-   }
+      Algorithms.reverse(grades);
    }
 
    /**
@@ -54,27 +47,8 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
       */
-      int i = grades.length-1;
-      if (grades != null && grades.length != 0){
-      for (int j=0; j<i; j++){
-         for (int k=0; k<i; k++){
-            if(grades[k]>grades[k+1]){
-               int tmp = grades[k];
-               grades[k] = grades[k+1];
-               grades[k+1] = tmp;
-            }
-         }
-      }
+      Algorithms.sort(grades);
    }
-   }
-      /*while (i > 0){
-         if (grades[i] < grades[i-1]) {
-            int tmp = grades[i];
-            grades[i] = grades[i-1];
-            grades[i-1] = tmp;
-         }
-      } 
-   }/* */
 
 
    /**
